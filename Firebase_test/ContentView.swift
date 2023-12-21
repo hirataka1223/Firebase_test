6f7c381a321faf508f2fad90abd554a0897d0962
hirataka1223 <154341912+hirataka1223@users.noreply.github.com>
2023-12-21 20:33:55 +0900

    Initial Commit

---
+++ Firebase_test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Firebase_test
+//
+//  Created by Taka on 2023/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firebase_test/Firebase_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_testApp.swift
+//  Firebase_test
+//
+//  Created by Taka on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
